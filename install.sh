27cec72972e4b75a15cd21415f12cec9029988ff
Gustavo Lichti <gustavo.lichti@gmail.com>
2013-08-10 18:04:12 -0300

    add pocket 1.5

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ SRC_APP=$SRC_PATH/applications
 SRC_ICONS=$SRC_PATH/icons
 SRC_THEMES=$SRC_PATH/themes
 SRC_EXTESIONS=$SRC_PATH/extensions
+SRC_BIN=$SRC_PATH/bin
 DST_APP=~/.local/share/applications
 DST_ICONS=~/.local/share/icons
 DST_THEMES=~/.themes
 DST_EXTENSIONS=~/.local/share/gnome-shell/extensions
+DST_BIN=~/bin
 
 function install(){
 	echo "Installing..."
@@ -22,6 +24,9 @@ function install(){
 
         make_dir $DST_EXTENSIONS
 	copy_files $SRC_EXTENSIONS $DST_EXTENSIONS "extensions"
+
+        make_dir $DST_BIN
+	copy_files $SRC_BIN $DST_BIN "bin"
 }
 
 function copy_files(){
